19e8e29ba8b931603e00ed548bb701737bb0e3b5
Brett Hales <brett@mytappr.com>
2017-01-16 13:14:38 +1000

    Basic Formatting

--- Open-Bank/ViewController.swift
+++ Open-Bank/ViewController.swift
@@ -14,25 +14,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        //Initialize a revealing Splash with with the iconImage, the initial size and the background color
-        let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "twitterLogo")!,iconInitialSize: CGSize(width: 70, height: 70), backgroundColor: UIColor(red:0.11, green:0.56, blue:0.95, alpha:1.0))
-        
-        //Adds the revealing splash view as a sub view
-        self.view.addSubview(revealingSplashView)
-        
-        //Starts animation
-        revealingSplashView.startAnimation(){
-            print("Completed")
-        }
-    }
+       
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    }
 
 }
 
