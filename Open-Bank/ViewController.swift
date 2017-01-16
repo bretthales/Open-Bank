38bb7a1b5fac9730de047216d97b3e4cec9a168c
Brett Hales <brett@mytappr.com>
2017-01-16 10:28:58 +1000

    Initial Commit

---
+++ Open-Bank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Open-Bank
+//
+//  Created by Brett Hales on 16/1/17.
+//  Copyright © 2017 Brett Hales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
